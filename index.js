0c5f3ab4007ac04fe83f2f3926eabbd1c61197da
github username <github email address>
2019-01-09 22:39:59 -0500

    increaseRankBy bug

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target")
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll(".ranked-list li")
+
+  list.forEach(function(liNum){
+    parseInt(liNum.innerText) + n
+  })
+}
